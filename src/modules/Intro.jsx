592ae5f4d930cfc1211d660975cce00e61f2a222
Mateusz Flisikowski <mateuszflisikowski86@icloud.com>
2022-12-20 12:42:48 +0100

    add font for headings

--- src/modules/Intro.jsx
+++ src/modules/Intro.jsx
@@ -1,7 +1,7 @@
 export function Intro({ title, intro }) {
   return (
     <>
-      <h1 className="max-w-xl text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+      <h1 className="font-cal text-4xl font-bold text-zinc-800 dark:text-zinc-100 sm:text-5xl">
         {title}
       </h1>
       <div className="mt-6 max-w-xl space-y-7 text-base text-zinc-600 dark:text-zinc-400">
--- src/modules/Layout.jsx
+++ src/modules/Layout.jsx
@@ -3,8 +3,8 @@ import { Container } from '@/modules/Container';
 export function Layout({ title, intro, children }) {
   return (
     <Container className="mt-16 sm:mt-32">
-      <header className="max-w-2xl">
-        <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+      <header className="max-w-2xl ">
+        <h1 className="font-cal text-4xl font-bold text-zinc-800 dark:text-zinc-100 sm:text-5xl">
           {title}
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
