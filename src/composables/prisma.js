7100d6c459f2b1e2bc06ee8217624aaf75b5829c
Nomads Codes <contact@nomads.codes>
2022-12-21 23:19:07 +0100

    Fix: There are already * instances of Prisma Client actively running.

---
+++ src/composables/prisma.js
@@ -0,0 +1,8 @@
+// https://www.prisma.io/docs/guides/database/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = global;
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
