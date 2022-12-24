cd5695c54dcbb3a52e3dbd0f26ed0193ce004184
Nomads Codes <contact@nomads.codes>
2022-12-24 14:48:15 +0100

    chore: rollback prisma export

--- src/composables/prisma.js
+++ src/composables/prisma.js
@@ -1,9 +1,6 @@
 // https://www.prisma.io/docs/guides/database/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global;
-
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient({ errorFormat: 'pretty' });
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export const prisma = new PrismaClient({
+  errorFormat: 'pretty',
+});
