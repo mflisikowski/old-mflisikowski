9b411e20689cadb4b540a60c25274c752f2e911d
Nomads Codes <contact@nomads.codes>
2022-12-29 16:54:28 +0100

    chore: add portrait image proptypes, update package json

---
+++ src/constants/props.js
@@ -0,0 +1,9 @@
+import { portraitImage } from './props/portrait-image';
+
+export const defaultTypes = {
+  portraitImage: portraitImage.types,
+};
+
+export const defaultProps = {
+  portraitImage: portraitImage.defaults,
+};
--- src/modules/PortraitImage.jsx
+++ src/modules/PortraitImage.jsx
@@ -1,17 +1,16 @@
+import { defaultProps, defaultTypes } from '@/constants/props';
 import { CustomImage } from '@/components/CustomImage';
 
-export function PortraitImage({ visibilityRegions = [], src }) {
-  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const isVisibile = visibilityRegions.includes(timeZone);
-
+export function PortraitImage({ src }) {
   return (
-    isVisibile && (
-      <div className="relative h-96 lg:h-auto">
-        <CustomImage
-          className="aspect-square rounded-xl bg-zinc-100 object-cover dark:bg-zinc-800 lg:rounded-2xl"
-          src={src}
-        />
-      </div>
-    )
+    <div className="relative h-96 lg:h-auto">
+      <CustomImage
+        className="aspect-square rounded-xl bg-zinc-100 object-cover dark:bg-zinc-800 lg:rounded-2xl"
+        src={src}
+      />
+    </div>
   );
 }
+
+PortraitImage.defaultProps = defaultProps.portraitImage;
+PortraitImage.propTypes = defaultTypes.portraitImage;
