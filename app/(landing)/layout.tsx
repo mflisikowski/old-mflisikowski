d227f63b401417714e328f753ad0f0cd60bf2b31
Mateusz Flisikowski <contact@mflisikowski.dev>
2023-08-13 16:00:26 +0200

    refactor(landing-layout): reorder GridPattern props for better readability

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -27,10 +27,10 @@ const LandingLayout = ({ children }: LandingLayoutProps) => {
       >
         <GridPattern
           className="absolute inset-x-0 -top-14 -z-10 h-[1000px] w-full fill-neutral-50 stroke-neutral-950/5 [mask-image:linear-gradient(to_bottom_left,white_40%,transparent_50%)]"
-          yOffset={-96}
-          interactive
           initial={{ opacity: 0, y: 100 }}
           animate={{ opacity: 1, y: 0 }}
+          yOffset={-96}
+          interactive
         />
 
         <Container layout>
