8eacf96436d9fae6c6cc0e9252027ff4db49f56c
Mateusz Flisikowski <contact@mflisikowski.dev>
2023-08-13 15:27:58 +0200

    feat(app): add NotFound page and refactor RootLayout import order and styling

---
+++ app/not-found.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="h-full flex items-center justify-center">
+      <div className="w-screen max-w-lg text-center">
+        <h2 className="text-7xl font-bold mb-4">404</h2>
+        <p className="text-xl mb-4">Nie znaleziono strony </p>
+        <p className="text-sm mb-4">
+          Wróć do{" "}
+          <Link
+            href="/"
+            className="underline underline-offset-2 hover:underline-offset-auto"
+          >
+            strony głównej
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
