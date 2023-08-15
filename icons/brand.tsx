6451c5ada0f0c7df44acfe09ac56508054153b0b
Mateusz Flisikowski <contact@mflisikowski.dev>
2023-08-15 18:38:45 +0200

    refactor(brand-icon): simplify Brand icon component
    - Moved the Brand component from `logotypes` to `icons` directory
    - Removed the framer-motion

--- icons/brand.tsx
+++ icons/brand.tsx
@@ -1,16 +1,5 @@
-import { motion } from "framer-motion";
-
 export const Brand = ({ ...props }) => (
-  <motion.div
-    initial={{ opacity: 0, y: -100 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{
-      ease: "easeInOut",
-      duration: 1,
-      delay: 0.8,
-    }}
-    {...props}
-  >
+  <div {...props}>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       preserveAspectRatio="none"
@@ -21,5 +10,5 @@ export const Brand = ({ ...props }) => (
     >
       <path d="m25.8 45.422-6.602-6.846-.334-.347a.502.502 0 0 0-.864.349V56.31H0V1.244A.502.502 0 0 1 .864.896l.336.348 24.6 25.512a1.667 1.667 0 0 0 2.4 0L52.796 1.249l.34-.353a.502.502 0 0 1 .864.348v55.067H36V38.578a.502.502 0 0 0-.864-.349l-.327.339-6.609 6.854a1.667 1.667 0 0 1-2.4 0Z" />
     </svg>
-  </motion.div>
+  </div>
 );
