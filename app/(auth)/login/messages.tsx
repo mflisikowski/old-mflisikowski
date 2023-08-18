784db826542905d67c68da0709687040d8ac7678
Mateusz Flisikowski <contact@mflisikowski.dev>
2023-08-18 19:50:33 +0200

    feat(auth): add login page, layout and messages components

---
+++ app/(auth)/login/messages.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+
+export default function Messages() {
+  const searchParams = useSearchParams();
+  const error = searchParams.get("error");
+  const message = searchParams.get("message");
+  return (
+    <>
+      {error && (
+        <p
+          className={`${
+            error ? "text-red-500" : "text-green-500"
+          } mt-4 p-4 bg-neutral-900 text-neutral-300 text-center`}
+        >
+          {error}
+        </p>
+      )}
+      {message && (
+        <p className="mt-4 p-4 bg-neutral-900 text-neutral-300 text-center">
+          {message}
+        </p>
+      )}
+    </>
+  );
+}
